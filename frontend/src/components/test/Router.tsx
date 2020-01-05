7805e66d5a4ce51b1e3695301250dd59ed8b9354
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-05 22:02:36 +0100

    adding routers

--- frontend/src/components/test/Router.tsx
+++ frontend/src/components/test/Router.tsx
@@ -4,43 +4,40 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default class LocalRouter extends Component {
     render() {
         return (
-          <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Hem</Link>
-                </li>
-                <li>
-                  <Link to="/gallery">Galleri</Link>
-                </li>
-                <li>
-                  <Link to="/about">Om oss</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Kontakt</Link>
-                </li>
-                <li>
-                  <Link to="/service">Tjänster och Utbud</Link>
-                </li>
-              </ul>
-            </nav>
-    
-            {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/about">
-                {/*<About />*/}
-              </Route>
-              <Route path="/users">
-                {/*<Users />*/}
-              </Route>
-              <Route path="/">
-                {/*<Home />*/}
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      );
+            <Router>
+                <div className="ui container center aligned">
+                    <div className="ui five big item secondary menu container" style={naviStyle}>
+                        <Link to="/" className="item">HEM</Link>
+                        <Link to="/gallery" className="item">GALLERI</Link>
+                        <Link to="/about" className="item">OM OSS</Link>
+                        <Link to="/contact" className="item">KONTAKT</Link>
+                        <Link to="/service" className="item">TJÄNSTER OCH UTBUD</Link>
+                    </div>
+                </div>
+        
+                {/* A <Switch> looks through its children <Route>s and
+                    renders the first one that matches the current URL. */}
+                <Switch>
+                <Route path="/about">
+                    {/*<About />*/}
+                </Route>
+                <Route path="/users">
+                    {/*<Users />*/}
+                </Route>
+                <Route path="/">
+                    {/*<Home />*/}
+                </Route>
+                </Switch>
+          
+            </Router>
+        );
     }
+}
+
+const naviStyle: React.CSSProperties = {
+    margin: 0,
+    padding: 0,
+    fontWeight: "normal",
+    color: "black",
+    fontFamily: "Montserrat"
 }
