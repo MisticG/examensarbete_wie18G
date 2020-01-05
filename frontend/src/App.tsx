242267ebbcc066cacc83ea74a180fb2cb72783a4
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-05 22:02:48 +0100

    adding fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import LocalRouter from './components/test/Router';
 export default class App extends Component {
     render() {
         return (
-          <div>
+          <div className="ui fluid container" style={{backgroundColor: "rgba(139, 108, 108, 0.9)"}}>
             <LocalRouter />
           </div>
         )
