5e5df5017716d40d87923b38d823f0ee04622190
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-06 13:46:07 +0100

    reusing one component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ import Header from './components/Header'
 import LatestNews from './components/LatestNews';
 import SectionDivider from './components/SectionDivider';
 import ServiceSection from './components/ServiceSection';
-import SectionDividerTwo from './components/SectionDividerTwo';
+import border_img from './assets/images/border_bg.jpg';
+import border_img2 from './assets/images/border_bg2.jpg';
 
 export default class App extends Component {
     render() {
@@ -13,12 +14,14 @@ export default class App extends Component {
           <div>
             <div style={{backgroundColor: "rgba(139, 108, 108, 0.9)"}}>
               <LocalRouter />
-        </div>
+            </div>
+            <div>
               <Header />
               <LatestNews />
-              <SectionDivider />
+              <SectionDivider image={border_img} />
               <ServiceSection />
-              <SectionDividerTwo />
+              <SectionDivider image={border_img2} />
+            </div>
           </div>
         )
     }
