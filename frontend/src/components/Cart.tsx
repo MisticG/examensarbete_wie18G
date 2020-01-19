5dd16850c39624dc2f996ad23cf5f2b2f6cb5dbe
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-19 23:59:01 +0100

    init cart comp

---
+++ frontend/src/components/Cart.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Cart extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <h1>hej från Cart</h1>
+            </React.Fragment>
+        )
+    }
+}
