2bf3b348c73ef05316b5a43157bb2ccc0e1db0d0
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-20 20:47:02 +0100

    Cart comp init

--- frontend/src/components/cart/Cart.tsx
+++ frontend/src/components/cart/Cart.tsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import CartColumns from './CartColumn';
+import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
     render() {
         return(
-            <React.Fragment>
-                <h1>hej från Cart</h1>
-            </React.Fragment>
+            <div className="ui fluid container" style={{textAlign: "center"}}>
+                <ProductConsumer>
+                {value => {
+                    const {cart} = value;
+                    if(cart.length>0) {
+                        return (
+                           <React.Fragment>
+                                <div className="header" style={{marginTop: "5em" }}>
+                                    <h1>Kundvagn</h1>
+                                </div>
+                                <CartColumns/>
+                                <CartList value={value} />
+                                <CartTotals value={value} />
+                            </React.Fragment>
+                        )
+                    } else {
+                        return <EmptyCart />
+                    }
+                }}
+                </ProductConsumer>
+            </div>
         )
     }
 }
