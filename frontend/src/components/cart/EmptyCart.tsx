88af4ebf840233c944686a98655e0c0c64aa1385
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-20 20:48:02 +0100

    EmptyCart comp init

---
+++ frontend/src/components/cart/EmptyCart.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EmptyCart() {
+    return (
+        <div className="ui container" >
+            <h1 style={{marginTop: "5em"}}>Din kundvagn är tom!</h1>
+        </div>
+    )
+}
