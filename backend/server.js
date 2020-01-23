8d6b39cf0e5c13a82766b6e93a9a4f4d5bc05969
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-23 05:05:53 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const postsRoute = require('./routes/products');
 const sendMessageRoute = require('./routes/sendMessage');
 const newsRoute = require('./routes/news');
@@ -9,6 +10,17 @@ app.use(express.urlencoded({
 }))
 
 app.use(express.json())
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/products', postsRoute);
 
@@ -21,6 +33,8 @@ app.get('/', (req, res) => {
     res.send('We are on home')
 });
 
-const port = 5000;
+//const port = 5000;
+const port = process.env.PORT || 8080;
+
 
 app.listen(port, () => console.log(`Server started port ${port}`));
