d374f03b2e3dd9aecb254c9bb8b892d3cfa02d61
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-23 19:02:44 +0100

    adding code

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,26 @@ const postsRoute = require('./routes/products');
 const sendMessageRoute = require('./routes/sendMessage');
 const newsRoute = require('./routes/news');
 
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
 app.use(express.urlencoded({
     extended: false
 }))
 
 app.use(express.json())
+
+//app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/products', postsRoute);
 
@@ -22,6 +37,6 @@ app.get('/', (req, res) => {
     res.send('We are on home')
 });
 
-const port = 5000;
+//const port = 5000;
 
 app.listen(port, () => console.log(`Server started port ${port}`));
