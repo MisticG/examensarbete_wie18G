10400c9bb0201f80aeae44df229f19953909fdff
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-23 05:21:55 +0100

    initial commit

---
+++ frontend/build/precache-manifest.d3b15784a797ef233b3ea91dc0d17ace.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0531e3ca9bc460a5847e6c2c1487f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2b036711191b51cab6",
+    "url": "/static/css/main.5d49617b.chunk.css"
+  },
+  {
+    "revision": "cfdaf6c40ed00be3fabe",
+    "url": "/static/js/2.315ed1f1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.315ed1f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd2b036711191b51cab6",
+    "url": "/static/js/main.0a90e9cd.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "208699218465040f8c07ea4643bc423a",
+    "url": "/static/media/DancingScript-Regular.20869921.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "b231f8373e2d66e417fd2b5902f65335",
+    "url": "/static/media/alana-harris-pink-flowers.b231f837.jpg"
+  },
+  {
+    "revision": "84dd33b00a5e8885129b93827b460dac",
+    "url": "/static/media/anita-jankovic-flowers.84dd33b0.jpg"
+  },
+  {
+    "revision": "166a02042924c3d0b5990332d771471d",
+    "url": "/static/media/artsy-vibes-bunch-flowers.166a0204.jpg"
+  },
+  {
+    "revision": "aa1bed59bd341b69a323f167acaa3132",
+    "url": "/static/media/bg_2.aa1bed59.jpg"
+  },
+  {
+    "revision": "e5377baf3f2042dfd5aaeac8f250ea7c",
+    "url": "/static/media/border_bg.e5377baf.jpg"
+  },
+  {
+    "revision": "e988d36a67271e17cd7db4529d730843",
+    "url": "/static/media/border_bg2.e988d36a.jpg"
+  },
+  {
+    "revision": "f703ec6e0f23be468f556607b34eb4cb",
+    "url": "/static/media/bouquet-691862_1920.f703ec6e.jpg"
+  },
+  {
+    "revision": "424e84db64ffe8cf010f401badea89c4",
+    "url": "/static/media/bride_bouquet.424e84db.jpg"
+  },
+  {
+    "revision": "bf7bde3236c22c26aa5d0db896817726",
+    "url": "/static/media/bukett_1.bf7bde32.jpg"
+  },
+  {
+    "revision": "a189def5cc7f694d7960224741eb7016",
+    "url": "/static/media/flower-393051_1920.a189def5.jpg"
+  },
+  {
+    "revision": "27fe41f87c0fec312e9be8242318e23b",
+    "url": "/static/media/ivan-di-tulip.27fe41f8.jpg"
+  },
+  {
+    "revision": "090e8c3b2f5eae564c86bf9882a156d8",
+    "url": "/static/media/kelly-sikkema.090e8c3b.jpg"
+  },
+  {
+    "revision": "84b322c86bccc5fc22580385eddf64eb",
+    "url": "/static/media/map.84b322c8.jpg"
+  },
+  {
+    "revision": "b61b7da4def99109296a3e4b175a3571",
+    "url": "/static/media/party_flowers.b61b7da4.jpg"
+  },
+  {
+    "revision": "ec37d44b9e07d01e6f5265d9756a20d0",
+    "url": "/static/media/paula-brustur-bridal.ec37d44b.jpg"
+  },
+  {
+    "revision": "94e15927e350bdad18d0d32ff57619f3",
+    "url": "/static/media/sad.94e15927.jpg"
+  },
+  {
+    "revision": "ed1e6e8d8caa7f39e369cf8df3b8b0dd",
+    "url": "/static/media/scotland_IoS.ed1e6e8d.jpg"
+  },
+  {
+    "revision": "41c27d8c7959f23013f8ab72533ca1d6",
+    "url": "/static/media/tracey-hocking.41c27d8c.jpg"
+  }
+]);
