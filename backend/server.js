ed2cc9b721b750f05a6508b4f7f69845b682fd15
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-24 01:28:34 +0100

    x

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const postsRoute = require('./routes/products');
 const sendMessageRoute = require('./routes/sendMessage');
 const newsRoute = require('./routes/news');
-
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
 
 app.use(express.urlencoded({
     extended: false
@@ -29,6 +24,6 @@ app.get('/', (req, res) => {
     res.send('We are on home')
 });
 
-//const port = 5000;
+const port = 5000;
 
 app.listen(port, () => console.log(`Server started port ${port}`));
