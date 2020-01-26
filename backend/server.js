6ce0ce3f0d370354ffccb2ff6e03a3381016aa98
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-26 20:23:52 +0100

    testng cert...

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,26 @@ const path = require('path');
 const cors = require('cors');
 require('dotenv/config');
 
+
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.backablommor.se/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/www.backablommor.se/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/www.backablommor.se/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+
 app.use(cors())
 
 app.use(express.urlencoded({extended: false}))
@@ -39,8 +59,21 @@ app.get('*', function(req, res) {
   res.set('Content-Type', 'text/css');
 });
 
-const port = process.env.PORT || 80;
+// Starting both http & https servers
 
-app.listen(port, () => console.log(`Server started port ${port}`));
+//const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+/*httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});*/
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+/*const port = process.env.PORT || 443;
+
+app.listen(port, () => console.log(`Server started port ${port}`));*/
 
 
