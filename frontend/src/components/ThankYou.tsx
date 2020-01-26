d2693ef6ce2e784691ae772a40c8ca08fa86ae47
misticG <tuy-vi.huynh@medieinstitutet.se>
2020-01-26 19:27:33 +0100

    Added thankyou component

---
+++ frontend/src/components/ThankYou.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ThankYou() {
+    return (
+        <div className="ui container" >
+            <h1 style={{marginTop: "5em"}}>Tack för att du handlade hos oss!</h1>
+            <h1>Välkommen åter!</h1>
+        </div>
+    )
+    
+}
